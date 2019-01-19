4bfe804d020a36d0092be6822c2c7e5a1699be8f
MaksymDranivskyi <max_dran@ukr.net>
2019-01-19 22:16:33 +0200

    no message

--- build.ps1
+++ build.ps1
@@ -27,7 +27,7 @@ $NugetExe = Join-Path $PSScriptRoot "nuget.exe"
 $MSBuildExe = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" 
 $Solution = Join-Path $PSScriptRoot "PhpTravels.UITests.sln"
 $NunitExe = "E:\NUnit.Console-3.9.0\nunit3-console.exe"
-$DebugFolder = Join-Path $PSScriptRoot "PhpTravels.UITests/bin/Debug"
+$DebugFolder = Join-Path $PSScriptRoot "\PhpTravels.UITests\bin\Debug"
 
 # Define additional variables here (MSBuild path, etc.)
 
@@ -90,6 +90,6 @@ foreach ($Task in $TaskList) {
     }
     if ($Task.ToLower() -eq 'copyartifacts')
     {
-        CopyBuildArtifacts "PhpTravels.UITests/bin/Debug" "$BuildArtifactsFolder"
+        CopyBuildArtifacts "$DebugFolder" "$BuildArtifactsFolder"
     }
 }
