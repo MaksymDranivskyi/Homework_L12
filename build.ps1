8c89fe602e0b5d75247427c29aef0965a287fed5
MaksymDranivskyi <max_dran@ukr.net>
2019-01-20 14:53:49 +0200

    no message

--- build.ps1
+++ build.ps1
@@ -25,7 +25,7 @@ param
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $NugetExe = & Join-Path $PSScriptRoot "nuget.exe"
 $MSBuildExe = & "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-$Solution =    & Join-Path $PSScriptRoot "PhpTravels.UITests.sln"
+$Solution = & Join-Path $PSScriptRoot "PhpTravels.UITests.sln"
 $NunitExe = "E:\NUnit.Console-3.9.0\nunit3-console.exe"
 $DebugFolder = & Join-Path $PSScriptRoot "PhpTravels.UITests\bin\Debug"
 $DebugFolder1 = & Join-Path $PSScriptRoot "PhpTravels.UITests\bin"
@@ -37,7 +37,7 @@ Function global:DownloadNuGet()
     if (-Not (Test-Path $NugetExe)) 
     {
         Write-Output "Installing NuGet from $NugetUrl..."
-        Invoke-WebRequest $NugetUrl -OutFile & $NugetExe -ErrorAction Stop
+        Invoke-WebRequest $NugetUrl -OutFile  $NugetExe -ErrorAction Stop
     }
 }
 
@@ -45,13 +45,13 @@ Function global:RestoreNuGetPackages()
 {
     DownloadNuGet
     Write-Output 'Restoring NuGet packages...'
-    Invoke-Expression & $NugetExe restore $Solution
+    Invoke-Expression  $NugetExe restore $Solution
 } 
 
 Function global:BuildSolution()
 {
     Write-Output "Building '$Solution' solution..."
-	Invoke-Expression & $MSBuild $Solution
+	Invoke-Expression  $MSBuild $Solution
     # MSBuild.exe call here
 }
 
